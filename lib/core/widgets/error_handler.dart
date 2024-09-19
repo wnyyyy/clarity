6e9f314fdf5cfc317fdf54b530e83d60ecfe1f92
Vinicius.Chaves <Vinicius.Chaves@Dell.com>
2024-09-19 20:04:05 -0300

    add some tests

---
+++ lib/core/widgets/error_handler.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ErrorHandler extends StatelessWidget {
+  final String? message;
+  final VoidCallback? onRetry;
+
+  const ErrorHandler({this.message, this.onRetry});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Oops! Algo deu errado aqui...",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          if (message != null) Text(message!),
+          if (onRetry != null)
+            ElevatedButton(
+              onPressed: () => onRetry!(),
+              child: const Text('Tentar novamente'),
+            ),
+        ],
+      ),
+    );
+  }
+}
